92f7b92a90ae03ca4f2b36476ab7e4f27e731bf3
Gothax <scott990328@gmail.com>
2025-06-25 17:22:35 +0900

    feat: 카카오 oauth 예외처리 추가

--- loopz-authentication-service/src/main/java/kr/co/loopz/authentication/exception/AuthenticationException.java
+++ loopz-authentication-service/src/main/java/kr/co/loopz/authentication/exception/AuthenticationException.java
@@ -1,15 +1,14 @@
 package kr.co.loopz.authentication.exception;
 
 import kr.co.loopz.common.exception.CustomException;
-import kr.co.loopz.common.exception.ErrorCode;
 
 public class AuthenticationException extends CustomException {
 
-    public AuthenticationException(ErrorCode errorCode) {
+    public AuthenticationException(AuthenticationErrorCode errorCode) {
         super(errorCode);
     }
 
-    public AuthenticationException(ErrorCode errorCode, String message) {
+    public AuthenticationException(AuthenticationErrorCode errorCode, String message) {
         super(errorCode, message);
     }
 }
