d8224c54ad8fb8d86769a9285da6ca671584e3bf
Gothax <scott990328@gmail.com>
2025-06-27 01:07:53 +0900

    fix: 카카오 소셜로그인 예외처리 추가

--- loopz-authentication-service/src/main/java/kr/co/loopz/authentication/dto/response/KakaoResourceServerResponse.java
+++ loopz-authentication-service/src/main/java/kr/co/loopz/authentication/dto/response/KakaoResourceServerResponse.java
@@ -1,11 +1,17 @@
 package kr.co.loopz.authentication.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record KakaoResourceServerResponse(
         Long id,
         @JsonProperty("kakao_account")
+        @NotNull @Valid
         KakaoAccount kakaoAccount,
+        @NotNull @Valid
         KakaoProperties properties
 ) {
     public record KakaoAccount(
@@ -17,10 +23,12 @@ public record KakaoResourceServerResponse(
             Boolean isEmailValid,
             @JsonProperty("is_email_verified")
             Boolean isEmailVerified,
+            @NotBlank @Email
             String email
     ) {}
 
     public record KakaoProperties(
+            @NotBlank
             String nickname,
             @JsonProperty("profile_image") String profileImage,
             @JsonProperty("thumbnail_image") String thumbnailImage
