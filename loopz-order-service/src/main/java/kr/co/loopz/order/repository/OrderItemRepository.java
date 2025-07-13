c5eabba8d2f27fc0227324fdec15ff9d71b0f3be
limdodod <rosalim2001@naver.com>
2025-07-13 19:56:21 +0900

    feat(order): Order Repository 추가

--- loopz-order-service/src/main/java/kr/co/loopz/order/repository/OrderItemRepository.java
+++ loopz-order-service/src/main/java/kr/co/loopz/order/repository/OrderItemRepository.java
@@ -3,5 +3,9 @@ package kr.co.loopz.order.repository;
 import kr.co.loopz.order.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findAllByOrderId(String orderId);
 }
