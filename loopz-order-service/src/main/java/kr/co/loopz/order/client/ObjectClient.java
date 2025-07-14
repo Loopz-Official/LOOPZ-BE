575da2ed4f5377ebfe29b987709e1474afb54136
limdodod <rosalim2001@naver.com>
2025-07-14 19:17:56 +0900

    feat(order): 상품 상세보기용 objectClient

--- loopz-order-service/src/main/java/kr/co/loopz/order/client/ObjectClient.java
+++ loopz-order-service/src/main/java/kr/co/loopz/order/client/ObjectClient.java
@@ -23,6 +23,9 @@ public interface ObjectClient {
     @PostMapping("/internal/objects")
     List<InternalObjectResponse> getObjectListByIds(@RequestBody List<String> requestIds);
 
+    @PostMapping("/internal/object/{objectId}")
+    InternalObjectResponse getObjectById(@PathVariable String objectId);
+
     // 주문 후 재고 감소
     @PostMapping("/internal/object/{objectId}/stock")
     void decreaseStock(@PathVariable String objectId, @RequestParam int quantity);
