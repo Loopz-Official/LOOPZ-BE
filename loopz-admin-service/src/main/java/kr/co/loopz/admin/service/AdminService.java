2a346c87a04650fdffc6e8c016d2816fe23c0629
limdodod <rosalim2001@naver.com>
2025-08-01 00:25:40 +0900

    feat(admin): feignException 추가

--- loopz-admin-service/src/main/java/kr/co/loopz/admin/service/AdminService.java
+++ loopz-admin-service/src/main/java/kr/co/loopz/admin/service/AdminService.java
@@ -1,11 +1,15 @@
 package kr.co.loopz.admin.service;
 
+import feign.FeignException;
 import kr.co.loopz.admin.client.ProductClient;
 import kr.co.loopz.admin.dto.request.UploadRequest;
 import kr.co.loopz.admin.dto.response.UploadResponse;
+import kr.co.loopz.admin.exception.AdminException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static kr.co.loopz.admin.exception.AdminErrorCode.DUPLICATE_OBJECT_NAME;
 
 
 @Service
@@ -15,8 +19,15 @@ public class AdminService {
 
     private final ProductClient productClient;
 
-    public UploadResponse uploadObject(String userId,UploadRequest request) {
-        return productClient.uploadObject(userId, request);
+    public UploadResponse uploadObject(String userId, UploadRequest request) {
+        try {
+            return productClient.uploadObject(userId, request);
+        } catch (FeignException.BadRequest e) {
+
+            log.error("상품 업로드 중 에러 발생: {}", e.contentUTF8(), e);
+
+            throw new AdminException(DUPLICATE_OBJECT_NAME, "objectName:"+request.objectName());
+        }
     }
 
 }
