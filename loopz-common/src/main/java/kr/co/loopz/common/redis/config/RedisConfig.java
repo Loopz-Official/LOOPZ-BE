f401a6846292b6715a8def902b0251dbd3b41515
Junhyung Kim <82752784+Gothax@users.noreply.github.com>
2025-07-31 13:57:49 +0900

    chore: Redis를 Elastic Cache로 이전 (#129)
    * chore: Elastic cache 환경변수 추가
    
    * chore: 두가지 클라이언트 SSL 설정
    
    - lettuce, redisson 모두 수정

--- loopz-common/src/main/java/kr/co/loopz/common/redis/config/RedisConfig.java
+++ loopz-common/src/main/java/kr/co/loopz/common/redis/config/RedisConfig.java
@@ -1,10 +1,16 @@
 package kr.co.loopz.common.redis.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -15,9 +21,40 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.ssl.enabled}")
+    private boolean sslEnabled;
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory factory() {
+
+        // standalone mode
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+
+        // socket options
+        SocketOptions socketOptions = SocketOptions.builder()
+                .connectTimeout(Duration.ofMillis(100L))
+                .keepAlive(true)
+                .build();
+
+        // client options
+        ClientOptions clientOptions = ClientOptions.builder()
+                .socketOptions(socketOptions)
+                .build();
+
+        // Lettuce Client option
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofMillis(3000L));
+
+        if (sslEnabled) {
+            builder.useSsl();
+        }
+
+        LettuceClientConfiguration clientConfiguration = builder.build();
+
+        return new LettuceConnectionFactory(configuration, clientConfiguration);
     }
 
 
