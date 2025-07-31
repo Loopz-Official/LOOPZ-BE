e67b09e43af175c0a009e9a4166fda106899e6e3
limdodod <rosalim2001@naver.com>
2025-07-31 15:50:17 +0900

    feat(admin): S3 추가

---
+++ loopz-admin-service/src/main/java/kr/co/loopz/admin/apiExternal/S3Controller.java
@@ -0,0 +1,31 @@
+package kr.co.loopz.admin.apiExternal;
+
+import kr.co.loopz.admin.dto.response.UrlResponse;
+import kr.co.loopz.admin.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s3/v1")
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/presignedUrl")
+    public ResponseEntity<UrlResponse> getPresignedUrl(@RequestParam String fileName) {
+
+        String presignedUrl = s3Service.generatePresignedUrl(fileName);
+
+        String imageKey = presignedUrl.substring(
+                presignedUrl.indexOf(".amazonaws.com/") + ".amazonaws.com/".length(),
+                presignedUrl.indexOf("?")
+        );
+
+        return ResponseEntity.ok(new UrlResponse(presignedUrl,imageKey));
+    }
+}
---
+++ loopz-admin-service/src/main/java/kr/co/loopz/admin/service/S3Service.java
@@ -0,0 +1,42 @@
+package kr.co.loopz.admin.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import kr.co.loopz.admin.dto.response.UrlResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.amazonaws.HttpMethod;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String generatePresignedUrl(String fileName) {
+        String uuidFileName =  UUID.randomUUID() + "_" + fileName;
+
+        Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 5);
+
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, uuidFileName)
+                .withMethod(HttpMethod.PUT)
+                .withExpiration(expiration);
+        request.addRequestParameter("Content-Type", "image/jpeg");
+
+        URL url = amazonS3Client.generatePresignedUrl(request);
+
+        return url.toString();
+    }
+}
