93d6e3b5a239e2be39941bc0b43c55ca4166b9bf
limdodod <rosalim2001@naver.com>
2025-07-31 15:47:35 +0900

    feat(admin): 업로드용 메서드 생성

--- loopz-product-service/src/main/java/kr/co/loopz/object/domain/ObjectDetail.java
+++ loopz-product-service/src/main/java/kr/co/loopz/object/domain/ObjectDetail.java
@@ -1,10 +1,16 @@
 package kr.co.loopz.object.domain;
 
 import jakarta.persistence.Embeddable;
+import kr.co.loopz.object.domain.enums.Keyword;
+import kr.co.loopz.object.domain.enums.ObjectSize;
+import kr.co.loopz.object.domain.enums.ObjectType;
 import kr.co.loopz.object.exception.ObjectException;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 import static kr.co.loopz.object.exception.ObjectErrorCode.INSUFFICIENT_STOCK;
 
@@ -31,5 +37,20 @@ public class ObjectDetail {
 
     public void increaseStock(int quantity) {
         this.stock += quantity;
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ObjectDetail(String size, String descriptionUrl, int stock) {
+        this.size = size;
+        this.descriptionUrl = descriptionUrl;
+        this.stock = stock;
+    }
+
+    public static ObjectDetail upload(String size, String descriptionUrl, int stock) {
+        return ObjectDetail.builder()
+                .size(size)
+                .descriptionUrl(descriptionUrl)
+                .stock(stock)
+                .build();
     }
 }
